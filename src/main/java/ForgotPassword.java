fa232e279dd45a0bc0b6f3bc42047ce688783eeb
Pravin Jadhav <66154438+pravinjadhav123@users.noreply.github.com>
2024-10-15 20:23:29 +0530

    Updatig forgot password

--- src/main/java/ForgotPassword.java
+++ src/main/java/ForgotPassword.java
@@ -1,3 +1,21 @@
-public class ForgotPassword {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class ForgotPassword {
+    @FindBy(id = "email")
+    WebElement txtemail;
+    @FindBy(xpath = "//button[@type='submit']") WebElement btnReset;
+
+    public ForgotPassword(WebDriver driver){
+        PageFactory.initElements(driver,this );
+    }
+
+    public void setTxtEmail(String email){
+        txtemail.sendKeys(email);
+    }
+    public void clickReset(){
+        btnReset.click();
+    }
 }
